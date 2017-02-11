0083ac650e41d1e03c1f2c1b88919b22fa14eb89
Zac Yu <me@zacyu.com>
2017-02-11 02:56:56 -0500

    Allow extracting tweets by handle

--- server.py
+++ server.py
@@ -1,4 +1,18 @@
 from flask import Flask
+from twython import Twython
+import ConfigParser
+import json
+
+Config = ConfigParser.ConfigParser()
+Config.read("config.ini")
+
+CONSUMER_KEY = Config.get('TwitterAPI', 'APIKey')
+CONSUMER_SECRET = Config.get('TwitterAPI', 'APISecret')
+ACCESS_KEY = Config.get('TwitterAPI', 'AccessKey')
+ACCESS_SECRET = Config.get('TwitterAPI', 'AccessSecret')
+
+twitter = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,7 +21,11 @@ def run():
 
 @app.route("/generate/<username>")
 def generate(username):
-    return "Generating lyrics for @" + username
+    result = ""
+    tweets = twitter.get_user_timeline(screen_name=username, count=200, include_rts=False)
+    for tweet in tweets:
+        result += tweet['text'] + "\n"
+    return result
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
